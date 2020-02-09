1f91ab7c87b3c1ddedf0594bbabc7ae65d74f1af
Garrett Woodall <garrett1woodall@gmail.com>
2020-02-09 15:40:34 -0600

    input

---
+++ src/app/google-maps-input/google-maps-input.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import PlaceResult = google.maps.places.PlaceResult;
+
+@Component({
+  selector: "app-google-maps-input",
+  templateUrl: "./google-maps-input.component.html",
+  styleUrls: ["./google-maps-input.component.css"]
+})
+export class GoogleMapsInputComponent implements OnInit {
+  public selectedAddress: PlaceResult;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAutocompleteSelected(result: PlaceResult) {
+    console.log("onAutocompleteSelected: ", result);
+    console.log(result);
+  }
+
+  onLocationSelected(location: Location) {
+    console.log("onLocationSelected: ", location);
+    console.log(location);
+  }
+}
