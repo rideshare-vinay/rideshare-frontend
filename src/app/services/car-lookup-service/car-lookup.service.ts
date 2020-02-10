a5609090563c1b69dabf8d4968d096b9e49ef17a
Reginald Uy <reuy@ucsd.edu>
2020-02-10 14:13:38 -0600

    Refactored Lookup-service

--- src/app/services/car-lookup-service/car-lookup.service.ts
+++ src/app/services/car-lookup-service/car-lookup.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class CarServiceService {
+export class CarLookupService {
   private CarDB:string
 
   constructor(private http:HttpClient) {
