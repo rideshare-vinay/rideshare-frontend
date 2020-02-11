4db2f095c1ceb16549b987fef71530f5d79d5f81
Baldemar Sepulveda <balde.sepu@pm.me>
2020-02-10 19:30:43 -0600

    Tested NgOnInit for MyCarComponent

--- src/app/components/my-car/my-car.component.spec.ts
+++ src/app/components/my-car/my-car.component.spec.ts
@@ -1,38 +1,101 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture, fakeAsync } from "@angular/core/testing";
 import { MyCarComponent } from './my-car.component';
-import { AdminComponent } from '../admin/admin.component';
-import { CarRegisterComponent } from '../car-register/car-register.component';
-import { RegisterComponent } from '../register/register.component';
-import { LoginComponent } from '../login/login.component';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { PreferenceComponent } from '../preference/preference.component';
-import { ProfileComponent } from '../profile/profile.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
+import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
+import { CarService } from 'src/app/services/car-service/car.service';
+import { Car } from 'src/app/models/car';
 
-describe('MyCarComponent', () => {
-  let component: MyCarComponent;
-  let fixture: ComponentFixture<MyCarComponent>;
+describe("MyCarComponent", () => {
+  let myCarComponent:MyCarComponent;
+  let myCarFixture:ComponentFixture<MyCarComponent>;
+  let mockAuthService:AuthService;
+  let mockCarService:CarService;
+  let routerSpy = jasmine.createSpyObj("Router", ['navigate']);
+  let mockUser:User; 
+  let mockCar:Car;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MyCarComponent, AdminComponent, CarRegisterComponent, RegisterComponent, LoginComponent, NavbarComponent, PreferenceComponent, ProfileComponent ],
-      imports: [HttpClientModule, AppRoutingModule, FormsModule],
-      providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}]
-    })
-    .compileComponents();
-  }));
+  class MockAuthService{
+    user:User;
+  }
+
+  class MockCarService{
+    getCarByUserId(userId:number){
+      return new Promise((resolve, reject) => {
+        resolve(mockCar);
+      });
+    }
+  }
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MyCarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [MyCarComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService }, 
+        { provide: Router, useValue: routerSpy },
+        { provide: CarService, useClass: MockCarService }
+      ]
+    });
+    myCarFixture = TestBed.createComponent(MyCarComponent); 
+    myCarComponent = myCarFixture.componentInstance; 
+    mockAuthService = TestBed.get(AuthService);
+    mockCarService = TestBed.get(CarService);
+    mockUser = {
+      userId: 1, 
+      userName: "killianC", 
+      firstName: "Killian", 
+      lastName: "Cumberbatch", 
+      phoneNumber: "5555555555", 
+      acceptingRides: false, 
+      active: true, 
+      batch: {batchLocation: "abc123", batchNumber: 123},
+      driver: false, 
+      email: "email@email.com"};
+    mockCar = {
+      carId: 1,
+      color: "red",
+      make: "Jumbo",
+      model: "T54",
+      seats: 5,
+      user: mockUser,
+      year: 2013
+    }
+  })
+
+  it("should create MyCarComponent", () => {
+    expect(myCarComponent).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should have no user set after construction", () => {
+    expect(myCarComponent.userId).toBeUndefined();
+  })
+
+  it("should set user id within MyCarComponent after Angular calls ngOnInit", () => {
+    mockAuthService.user = mockUser;
+    myCarComponent.ngOnInit();
+    expect(myCarComponent.userId).toEqual(mockUser.userId);
+  });
+
+  it("should redirect if userid is set to zero", () => {
+    mockUser.userId = 0;
+    mockAuthService.user = mockUser;
+    myCarComponent.ngOnInit();
+
+    const spy = routerSpy.navigate as jasmine.Spy;
+    const navArgs = spy.calls.first().args[0];
+
+    expect(navArgs).toEqual([""]);
+  });
+
+  it("should set myCar for the MyComponent if CarService gets valid user id", (done) => {
+    mockAuthService.user = mockUser;
+    myCarComponent.ngOnInit();
+    mockCarService.getCarByUserId(mockUser.userId).then( car => {
+      expect(myCarComponent.myCar).toEqual(mockCar);
+      done();
+    });
   });
 });
