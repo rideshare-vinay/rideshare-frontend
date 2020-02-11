d0bff7bd3c82217ffb18ce75e530f86a01c597b0
Remedee <zacharydanieldowns@gmail.com>
2020-02-11 14:40:06 -0600

    Started some documentation

--- src/app/services/google-maps-service/google-maps.service.ts
+++ src/app/services/google-maps-service/google-maps.service.ts
@@ -7,40 +7,58 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class GoogleMapsService {
+  /**
+   *
+   */
   private googleMapsInputVisibility: boolean = true;
-  private showMarker: boolean = true;
+  private showMarkerOrCircle: boolean = true;
   private url: string;
 
   // prettier-ignore
   public googleMapsInputVisibilityEvent: BehaviorSubject<boolean> =
     new BehaviorSubject<boolean>(this.googleMapsInputVisibility);
   // prettier-ignore
-  public showMarkerEvent: BehaviorSubject<boolean> =
-    new BehaviorSubject<boolean>(this.showMarker);
+  public showMarkerOrCircleEvent: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(this.showMarkerOrCircle);
 
+  /**
+   * Sets the url to the location of the GoogleMapsController in the User Service backend.
+   * @param http Injection of HttpClient for calls to the GoogleMapsController.
+   */
   constructor(private http: HttpClient) {
     this.url = environment.rootUri;
   }
 
-  // A setter method for changing the visibility of the google maps input bar.
+  /**
+   * A setter method for changing the visibility of the google maps input bar.
+   * @param visibility True makes the input bar visible, false makes it invisible.
+   */
   public setInputVisibility(visibility: boolean): void {
     this.googleMapsInputVisibility = visibility;
     this.googleMapsInputVisibilityEvent.next(this.googleMapsInputVisibility);
   }
 
-  // A getter method for the visibility of the google maps input bar.
+  /**
+   * A getter method for the visibility of the google maps input bar.
+   */
   public getInputVisibility(): boolean {
     return this.googleMapsInputVisibility;
   }
 
-  // A setter method for choosing whether to show a marker or circle.
-  public setShowMarker(show: boolean): void {
-    this.showMarker = show;
-    this.showMarkerEvent.next(this.showMarker);
+  /**
+   * A setter method for choosing whether to show a marker or circle.
+   * @param show True makes the marker visible, false makes the circle visible.
+   */
+  public setShowMarkerOrCircle(show: boolean): void {
+    this.showMarkerOrCircle = show;
+    this.showMarkerOrCircleEvent.next(this.showMarkerOrCircle);
   }
 
-  // A getter method for whether a marker is shown.
-  public getShowMarker(): boolean {
-    return this.showMarker;
+  /**
+   * A getter method for whether a marker or circle is shown.
+   * true == marker and false == circle
+   */
+  public getShowMarkerOrCircle(): boolean {
+    return this.showMarkerOrCircle;
   }
 }
