b3c3ca4fd7b945839f47475c8c5a51cba50b8b15
Remedee <zacharydanieldowns@gmail.com>
2020-02-11 13:37:11 -0600

    Default Angular Tab Width is 2

--- src/app/services/google-maps-service/google-maps.service.ts
+++ src/app/services/google-maps-service/google-maps.service.ts
@@ -4,45 +4,43 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class GoogleMapsService {
-    private googleMapsInputVisibility: boolean = true;
-    private showMarker: boolean = true;
-    private url: string;
+  private googleMapsInputVisibility: boolean = true;
+  private showMarker: boolean = true;
+  private url: string;
 
-    // prettier-ignore
-    public googleMapsInputVisibilityEvent: BehaviorSubject<boolean> =
+  // prettier-ignore
+  public googleMapsInputVisibilityEvent: BehaviorSubject<boolean> =
     new BehaviorSubject<boolean>(this.googleMapsInputVisibility);
-    // prettier-ignore
-    public showMarkerEvent: BehaviorSubject<boolean> =
+  // prettier-ignore
+  public showMarkerEvent: BehaviorSubject<boolean> =
     new BehaviorSubject<boolean>(this.showMarker);
 
-    constructor(private http: HttpClient) {
-        this.url = environment.rootUri;
-    }
+  constructor(private http: HttpClient) {
+    this.url = environment.rootUri;
+  }
 
-    // A setter method for changing the visibility of the google maps input bar.
-    public setInputVisibility(visibility: boolean): void {
-        this.googleMapsInputVisibility = visibility;
-        this.googleMapsInputVisibilityEvent.next(
-            this.googleMapsInputVisibility
-        );
-    }
+  // A setter method for changing the visibility of the google maps input bar.
+  public setInputVisibility(visibility: boolean): void {
+    this.googleMapsInputVisibility = visibility;
+    this.googleMapsInputVisibilityEvent.next(this.googleMapsInputVisibility);
+  }
 
-    // A getter method for the visibility of the google maps input bar.
-    public getInputVisibility(): boolean {
-        return this.googleMapsInputVisibility;
-    }
+  // A getter method for the visibility of the google maps input bar.
+  public getInputVisibility(): boolean {
+    return this.googleMapsInputVisibility;
+  }
 
-    // A setter method for choosing whether to show a marker or circle.
-    public setShowMarker(show: boolean): void {
-        this.showMarker = show;
-        this.showMarkerEvent.next(this.showMarker);
-    }
+  // A setter method for choosing whether to show a marker or circle.
+  public setShowMarker(show: boolean): void {
+    this.showMarker = show;
+    this.showMarkerEvent.next(this.showMarker);
+  }
 
-    // A getter method for whether a marker is shown.
-    public getShowMarker(): boolean {
-        return this.showMarker;
-    }
+  // A getter method for whether a marker is shown.
+  public getShowMarker(): boolean {
+    return this.showMarker;
+  }
 }
