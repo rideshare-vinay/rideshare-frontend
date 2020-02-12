344edb1ad88a4cfbabd5c44144c94a27b8079773
John-Paul Vargas <jp.vargas@live.com>
2020-02-12 00:49:56 -0600

    batch and admin service tested, working on profile

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -12,6 +12,11 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { APP_BASE_HREF } from '@angular/common';
 import { CarRegisterComponent } from '../car-register/car-register.component';
+import { User } from 'src/app/models/user';
+import { Batch } from 'src/app/models/batch';
+import { DriverInfoComponent } from '../driver-info/driver-info.component';
+import { DriverComponent } from '../driver/driver.component';
+import { AdminLoginComponent } from '../admin-login/admin-login.component';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -19,7 +24,9 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent, AdminComponent, CarRegisterComponent, RegisterComponent, LoginComponent, MyCarComponent, NavbarComponent, PreferenceComponent ],
+      declarations: [AdminComponent, DriverInfoComponent, DriverComponent, LoginComponent, 
+        AdminLoginComponent, RegisterComponent, CarRegisterComponent, MyCarComponent,
+        ProfileComponent, PreferenceComponent ],
       imports: [HttpClientModule, AppRoutingModule, FormsModule],
       providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}]
     })
@@ -35,4 +42,27 @@ describe('ProfileComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return user info', () => {
+    let mockUser: User = {userId:1, userName: "testing", batch: new Batch(), 
+    firstName:"jon", lastName:"smith", email:"test", phoneNumber:"also a test",
+    active: true, driver: true, acceptingRides: true}
+    let spy: any;
+    spy = spyOn(component,'getUserInfo');
+
+    component.getUserInfo();
+    expect(spy).toHaveBeenCalled();
+   })
+
+   it('should compare user info', () => {
+    let mockUser: User = {userId:1, userName: "testing", batch: new Batch(), 
+    firstName:"jon", lastName:"smith", email:"test", phoneNumber:"also a test",
+    active: true, driver: true, acceptingRides: true}
+    let spy: any;
+    spy = spyOn(component, 'compareUser');
+
+    component.compareUser();
+    expect(spy).toHaveBeenCalled();
+   })
+   
 });
