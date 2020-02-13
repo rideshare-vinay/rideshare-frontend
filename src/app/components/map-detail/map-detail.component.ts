928c31bd09fca2a0857a696f955d125d762550ef
Remedee <zacharydanieldowns@gmail.com>
2020-02-13 12:20:52 -0600

    Added documentation to map-detail.component.ts

--- src/app/components/map-detail/map-detail.component.ts
+++ src/app/components/map-detail/map-detail.component.ts
@@ -12,9 +12,26 @@ import { Coordinates } from 'src/app/models/coordinates';
   styleUrls: ['./map-detail.component.css']
 })
 export class MapDetailComponent implements OnInit {
-  private recommendations: User[];
+  /**
+   * The max number of recommendations to get for a rider.
+   */
+  private numberOfRecommendations: number = 5;
+  /**
+   * The current logged in user, used to pass information to the user-service.
+   */
   private user: User = new User();
+  /**
+   * The recommended drivers returned as a list of Users.
+   */
+  public recommendations: User[];
 
+  /**
+   * A constructor to inject the necessary services.
+   * @param googleMapsService Used to pass markers to google-maps.component.ts
+   * @param authService Used to get the userId of the current user.
+   * @param userService Used to get the rest of a user's information.
+   * @param router Used to route back to login if no logged in user.
+   */
   constructor(
     private googleMapsService: GoogleMapsService,
     private authService: AuthService,
@@ -22,6 +39,9 @@ export class MapDetailComponent implements OnInit {
     private router: Router
   ) {}
 
+  /**
+   * OnInit get the userId if it exists and getUserInfo().
+   */
   ngOnInit() {
     this.user.userId = this.authService.user.userId;
     if (!this.user.userId) {
@@ -31,28 +51,41 @@ export class MapDetailComponent implements OnInit {
     }
   }
 
+  /**
+   * Get all the information of the logged in user using their userId. Then getDriverRecommendations().
+   */
   public getUserInfo() {
     this.userService.getUserById(this.user.userId).then(user => {
       if (user) {
         this.user = user;
-
-        this.googleMapsService
-          .getDriverRecommendations(5, user)
-          .subscribe(recommendations => {
-            this.recommendations = recommendations;
-
-            const markerList: Coordinates[] = [];
-            for (const recommendation of recommendations) {
-                const marker: Coordinates = new Coordinates(recommendation.latitude, recommendation.longitude);
-                markerList.push(marker);
-              }
-
-            this.googleMapsService.setCoordinatesList(markerList);
-          });
+        this.getDriverRecommendations(this.numberOfRecommendations, this.user);
       } else {
         this.authService.user = {};
         this.router.navigate(['']);
       }
     });
+  }
+
+  /**
+   * Get driver recommendations from the user-service recommendation algorithm.
+   * @param rider The user to get recommendations for.
+   */
+  public getDriverRecommendations(numOfRecommendations: number, rider: User) {
+    this.googleMapsService
+      .getDriverRecommendations(numOfRecommendations, rider)
+      .subscribe(recommendations => {
+        this.recommendations = recommendations;
+
+        const markerList: Coordinates[] = [];
+        for (const recommendation of recommendations) {
+          const marker: Coordinates = new Coordinates(
+            recommendation.latitude,
+            recommendation.longitude
+          );
+          markerList.push(marker);
+        }
+
+        this.googleMapsService.setCoordinatesList(markerList);
+      });
   }
 }
