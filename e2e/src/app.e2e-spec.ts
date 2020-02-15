ad11525948f3c7c59dd9f3f2b94edf677044ed0a
Annie Vo <annievo.av@hotmail.com>
2020-02-14 20:23:38 -0600

    e2e /login added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,58 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('App /login E2E tests', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('rideshare-frontend app is running!');
+  it('should display topBorder', () => {
+    expect(page.getTopBorder().isDisplayed()).toBeTruthy();
+  });
 
+  it('should display image', () => {
+    expect(page.getImage().isDisplayed()).toBeTruthy();
+  });
 
-    browser.sleep(2000);
+  it('should display login Card', () => {
+    expect(page.getLoginCard().isDisplayed()).toBeTruthy();
+  });
+
+  describe('navbarRideshare specific', () => {
+    it('should display navbarRideshare', () => {
+      expect(page.getNavbarRideshare().isDisplayed()).toBeTruthy();
+    });
+    it('signin link must clickable', () => {
+      page.getSignInLink().click().then(() => {
+        browser.sleep(3000).then(() => {
+          browser.getCurrentUrl().then((actualUrl) => {
+            expect(actualUrl.indexOf('login') !== -1).toBeTruthy();
+          });
+        });
+      });
+    });
+    it('signup link must clickable', () => {
+      browser.ignoreSynchronization = true;
+      page.getSignUpLink().click().then(() => {
+        browser.getCurrentUrl().then((actualUrl) => {
+          expect(actualUrl.indexOf('register') !== -1).toBeTruthy();
+        });
+      });
+    });
+    it('admin link must clickable', () => {
+      browser.ignoreSynchronization = true;
+      page.getAdminLink().click().then(() => {
+        browser.getCurrentUrl().then((actualUrl) => {
+          expect(actualUrl.indexOf('login/admin') !== -1).toBeTruthy();
+        });
+      });
+    });
   });
 
   afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,37 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get('login');
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getNavbarRideshare() {
+    return element(by.id('navbarRideshare'));
   }
+
+  getTopBorder() {
+    return element(by.className('topBorder'));
+  }
+
+  getImage() {
+    return element(by.className('responsive'));
+  }
+
+  getLoginCard() {
+    return element(by.className('loginCard'));
+  }
+
+  getSignInLink() {
+    return element(by.partialLinkText('SIGN IN'));
+  }
+
+  getSignUpLink() {
+    return element(by.partialLinkText('SIGN UP'));
+  }
+
+  getAdminLink() {
+    return element(by.partialLinkText('ADMIN'));
+  }
+  
 }
