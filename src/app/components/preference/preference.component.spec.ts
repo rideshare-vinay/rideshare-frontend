7dba84b3796aacfe9eaa7ab8a3166668e10fa362
JP204 <jp.vargas@live.com>
2020-02-16 00:10:50 -0600

    working on preference

--- src/app/components/preference/preference.component.spec.ts
+++ src/app/components/preference/preference.component.spec.ts
@@ -20,30 +20,51 @@ import { AdminLoginComponent } from '../admin-login/admin-login.component';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { User } from 'src/app/models/user';
+import { GoogleMapsComponent } from '../google-maps/google-maps.component';
 
-describe('PreferenceComponent', () => {
+fdescribe('PreferenceComponent', () => {
   let preferenceComponent: PreferenceComponent;
   let preferenceFixture: ComponentFixture<PreferenceComponent>;
-  let mockAuthService:AuthService;
+  let mockAuthService: AuthService;
+  let mockUser: User;
   let routerSpy = jasmine.createSpyObj("Router", ['navigate'])
 
-  class MockAuthService{
-    user:User;
+  class MockAuthService {
+    user: User;
   }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-      declarations: [ PreferenceComponent, AdminComponent, 
-        CarRegisterComponent, RegisterComponent, 
-        LoginComponent, MyCarComponent, 
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [PreferenceComponent, AdminComponent,
+        CarRegisterComponent, RegisterComponent,
+        LoginComponent, MyCarComponent,
         NavbarComponent, ProfileComponent,
         DriverComponent, MapDetailComponent,
-        DriverInfoComponent, AdminLoginComponent ],
+        DriverInfoComponent, AdminLoginComponent,
+        GoogleMapsComponent],
       imports: [HttpClientModule, AppRoutingModule, FormsModule],
-      providers: [{ provide: Router, useValue: routerSpy }]
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthService, useClass: MockAuthService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+    mockUser = {
+      userId: 1,
+      userName: "testChange",
+      batch: { batchLocation: 'UTA - Arlington, TX', batchNumber: 2 },
+      firstName: "Jon",
+      lastName: "Smith",
+      email: "test@test.com",
+      phoneNumber: "123-456-7890",
+      active: true,
+      driver: true,
+      acceptingRides: true,
+      address: "123 liv+",
+      latitude: 45,
+      longitude: 45
+    };
   }));
 
   beforeEach(() => {
@@ -56,6 +77,8 @@ describe('PreferenceComponent', () => {
     expect(preferenceComponent).toBeTruthy();
   });
 
-  // it('should get the users preference when running ngOnInit')
+  it("should route away from the page if the user isn't authorized", () => {
+    mockAuthService.user = mockUser;
+  })
 
 });
